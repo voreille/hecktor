e074ec8513852921db184f78c219d3d80635d8df
val <val@adrien2.hadoop-cluster.iig>
2020-06-10 12:31:26 +0200

    add comment to evaluate_preidctions.ipynb

--- src/evaluation/scores.py
+++ src/evaluation/scores.py
@@ -1,16 +1,6 @@
 import numpy as np
 
 
-def dice2d(mask_gt, mask_seg):
-    dice = 0
-    for i in range(np.shape(mask_gt)[2]):
-        dice += np.nan_to_num(
-            2 * np.sum(np.logical_and(mask_gt[:, :, i], mask_seg[:, :, i])) /
-            (np.sum(mask_gt[:, :, i]) + np.sum(mask_seg[:, :, i])))
-    dice /= np.shape(mask_gt)[2]
-    return dice
-
-
 def dice(mask_gt, mask_seg):
     return 2 * np.sum(np.logical_and(
         mask_gt, mask_seg)) / (np.sum(mask_gt) + np.sum(mask_seg))
