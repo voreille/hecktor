6973aa045528f7143799078d52a6cc69e2d772f7
voreille <valentin.oreiller@gmail.com>
2021-07-08 13:27:06 +0200

    add Hausdorff distance

--- src/evaluation/scores.py
+++ src/evaluation/scores.py
@@ -1,6 +1,27 @@
 import numpy as np
+from scipy.spatial import cKDTree
 
 
 def dice(mask_gt, mask_seg):
     return 2 * np.sum(np.logical_and(
         mask_gt, mask_seg)) / (np.sum(mask_gt) + np.sum(mask_seg))
+
+
+def hausdorff_distance(image0, image1):
+    """Code copied from 
+    https://github.com/scikit-image/scikit-image/blob/main/skimage/metrics/set_metrics.py#L7-L54
+    for compatibility reason with python 3.6
+    """
+    a_points = np.transpose(np.nonzero(image0))
+    b_points = np.transpose(np.nonzero(image1))
+
+    # Handle empty sets properly:
+    # - if both sets are empty, return zero
+    # - if only one set is empty, return infinity
+    if len(a_points) == 0:
+        return 0 if len(b_points) == 0 else np.inf
+    elif len(b_points) == 0:
+        return np.inf
+
+    return max(max(cKDTree(a_points).query(b_points, k=1)[0]),
+               max(cKDTree(b_points).query(a_points, k=1)[0]))
