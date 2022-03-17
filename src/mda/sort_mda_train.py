395b07cb5529a769a51fd60555df169bd08980b7
Valentin Oreiller <valentin.oreiller@gmail.com>
2022-03-17 11:52:10 +0100

    from dell

---
+++ src/mda/sort_mda_train.py
@@ -0,0 +1,20 @@
+import logging
+
+import click
+from okapy.dicomconverter import dicom_walker
+
+
+@click.command()
+@click.argument('input_folder', type=click.Path(exists=True), default=path_in)
+@click.argument('output_folder', type=click.Path(), default=path_out)
+def main():
+    logger = logging.getLogger(__name__)
+    logger.info('Resampling')
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logging.captureWarnings(True)
+
+    main()
