e13a0d3520e4c7bf1f8251e547caf76ddfbd8a4e
voreille <valentin.oreiller@gmail.com>
2022-05-17 08:42:17 +0200

    start 2022

--- src/data/make_dataset_2022.py
+++ src/data/make_dataset_2022.py
@@ -16,52 +16,46 @@ from src.data.utils import (correct_names, move_extra_vois, clean_vois,
                             compute_bbs)
 
 project_dir = Path(__file__).resolve().parents[2]
-default_input_path = project_dir / "hecktor/data/hecktor2022/raw/mda_test"
-default_images_folder = project_dir / "data/hecktor2021/hecktor_nii/"
-default_files_folder = project_dir / "data/hecktor2021/"
-default_archive = project_dir / "data/extra_voi"
+# default_input_path = project_dir / "hecktor/data/hecktor2022/raw/mda_test"
+default_input_path = "/mnt/nas4/datasets/ToReadme/HECKTOR/HECKTOR2022/dicom/mda_test"
+default_images_folder = project_dir / "data/hecktor2022/processed/mda_test/images"
+default_labels_folder = project_dir / "data/hecktor2022/processed/mda_test/labels"
+default_dump = project_dir / "data/hecktor2022/processed/mda_test/labels"
 default_name_mapping = project_dir / "data/hecktor2021_name_mapping_testing.csv"
-default_bb_file = project_dir / "data/hecktor2021/hecktor2021_bbox_training.csv"
 
 
 @click.command()
-@click.argument('input_folder',
-                type=click.Path(exists=True),
-                default=default_input_path)
+@click.argument('input_folder', type=click.Path(), default=default_input_path)
 @click.argument('output_images_folder',
                 type=click.Path(),
                 default=default_images_folder)
-@click.argument('output_files_folder',
+@click.argument('output_labels_folder',
                 type=click.Path(),
-                default=default_files_folder)
-@click.argument('bb_file', type=click.Path(), default=default_bb_file)
-@click.option("--archive_folder",
-              "-a",
-              type=click.Path(),
-              default=default_archive)
+                default=default_labels_folder)
+@click.argument('dump_folder', type=click.Path(), default=default_dump)
 @click.option("--name_mapping",
               type=click.Path(),
               default=default_name_mapping)
-def main(input_folder, output_images_folder, output_files_folder, bb_file,
-         archive_folder, name_mapping):
+def main(input_folder, output_images_folder, output_labels_folder, dump_folder,
+         name_mapping):
     """Command Line Interface to make the dataset for the HECKTOR Challenge
         In short, this routine convert the DICOM files to NIFTI and stores the CT in 
         Hounsfield Unit and the PET in Standardized Uptake value.
     """
 
     output_images_folder = Path(output_images_folder)
-    output_files_folder = Path(output_files_folder)
-    archive_folder = Path(archive_folder)
+    output_labels_folder = Path(output_labels_folder)
+    dump_folder = Path(dump_folder)
     output_images_folder.mkdir(exist_ok=True, parents=True)
-    archive_folder.mkdir(exist_ok=True, parents=True)
-    # logger.info("Converting Dicom to Nifty - START")
-    # converter = NiftiConverter(
-    #     padding="whole_image",
-    #     list_labels=["GTVt", "GTVn"],
-    #     cores=10,
-    #     naming=2,
-    # )
-    # _, _ = converter(input_folder, output_folder=output_images_folder)
+    dump_folder.mkdir(exist_ok=True, parents=True)
+    logger.info("Converting Dicom to Nifty - START")
+    converter = NiftiConverter(
+        padding="whole_image",
+        labels_startswith="GTV",
+        cores=24,
+        naming=2,
+    )
+    _ = converter(input_folder, output_folder=output_images_folder)
 
     logger.info("Converting Dicom to Nifty - END")
     # logger.info("Removing extra VOI - START")
@@ -71,13 +65,8 @@ def main(input_folder, output_images_folder, output_files_folder, bb_file,
     # correct_names(output_images_folder, name_mapping)
     # logger.info("Renaming files- END")
     # logger.info("Cleaning the VOIs - START")
-    clean_vois(output_images_folder)
-    logger.info("Cleaning the VOIs - END")
-
-    # logger.info("Computing the bounding boxes - START")
-    # bb_df = compute_bbs(output_images_folder)
-    # bb_df.to_csv(bb_file)
-    # logger.info("Computing the bounding boxes - END")
+    # clean_vois(output_images_folder)
+    # logger.info("Cleaning the VOIs - END")
 
 
 if __name__ == '__main__':
