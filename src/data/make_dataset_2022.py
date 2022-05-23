c131809f345f13db04643036d86f4dde771c66b1
voreille <valentin.oreiller@gmail.com>
2022-05-23 17:17:12 +0200

    from desuto

--- src/data/make_dataset_2022.py
+++ src/data/make_dataset_2022.py
@@ -11,18 +11,22 @@ logger = logging.getLogger(__name__)
 import click
 import pandas as pd
 from okapy.dicomconverter.converter import NiftiConverter
+from okapy.dicomconverter.dicom_walker import DicomWalker
 
 from src.data.utils import (correct_names, move_extra_vois, clean_vois,
                             compute_bbs)
 
 project_dir = Path(__file__).resolve().parents[2]
 # default_input_path = project_dir / "hecktor/data/hecktor2022/raw/mda_test"
-default_input_path = "/mnt/nas4/datasets/ToReadme/HECKTOR/HECKTOR2022/dicom/mda_test"
-default_images_folder = project_dir / "data/hecktor2022/processed/mda_test/images"
-default_labels_folder = project_dir / "data/hecktor2022/processed/mda_test/labels"
-default_dump = project_dir / "data/hecktor2022/processed/mda_test/labels"
+center ="CHB"
+default_input_path = f"/mnt/nas4/datasets/ToReadme/HECKTOR/HECKTOR2022/dicom/{center}"
+default_images_folder = project_dir / f"data/hecktor2022/processed/{center}/images"
+default_labels_folder = project_dir / f"data/hecktor2022/processed/{center}/labels"
+default_dump = project_dir / f"data/hecktor2022/processed/{center}/labels"
 default_name_mapping = project_dir / "data/hecktor2021_name_mapping_testing.csv"
 
+def filter_func(study):
+    return study.patient_id == "CHB003"
 
 @click.command()
 @click.argument('input_folder', type=click.Path(), default=default_input_path)
@@ -52,7 +56,9 @@ def main(input_folder, output_images_folder, output_labels_folder, dump_folder,
     converter = NiftiConverter(
         padding="whole_image",
         labels_startswith="GTV",
-        cores=24,
+        dicom_walker=DicomWalkerWithFilter(filter_func=filter_func, cores=24),
+        # cores=24,
+        cores=None,
         naming=2,
     )
     _ = converter(input_folder, output_folder=output_images_folder)
@@ -68,6 +74,14 @@ def main(input_folder, output_images_folder, output_labels_folder, dump_folder,
     # clean_vois(output_images_folder)
     # logger.info("Cleaning the VOIs - END")
 
+class DicomWalkerWithFilter(DicomWalker):
+    def __init__(self, input_dirpath=None, cores=None, additional_dicom_tags=None, submodalities=False, filter_func=None):
+        super().__init__(input_dirpath, cores, additional_dicom_tags, submodalities)
+        self.filter_func = filter_func
+
+    def __call__(self, input_dirpath=None, cores=None):
+        studies = super().__call__(input_dirpath, cores) 
+        return [s for s in studies if self.filter_func(s)]
 
 if __name__ == '__main__':
     main()
